126f768dcf70e4a093d7d67dc9b6b3dcf578f1cb
Rajesh Polukonda <rajeshpolukonda5@gmail.com>
2024-05-06 17:07:21 +0530

    initial commit

---
+++ src/main/java/com/nt/service/App.java
@@ -0,0 +1,17 @@
+package com.nt.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public int sum(int x,int y)
+	{
+	 return x+y;
+	}
+    public static void main( String[] args )
+    {
+        System.out.println("sum is ::"+new App().sum(10,20));
+    }
+}
