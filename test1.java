8d7436e497983d6e0d5f0a577eed279aa9ef48bc
Rajesh Polukonda <rajeshpolukonda5@gmail.com>
2024-05-10 17:15:43 +0530

    dev1-added m2() method

--- test1.java
+++ test1.java
@@ -2,7 +2,11 @@ public class Test1
 {
 public void m1()
 {
- System.out.println("welcome to ma()");
+ System.out.println("welcome to m1()");
+}
+public void m1()
+{
+ System.out.println("welcome to m2()");
 }
 public static void main(String args[])
 {
